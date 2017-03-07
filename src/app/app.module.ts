8cf0606bba1fa1aa46c75d92a02f7a4fed3aee6c
cherish2016 <suchang0203@163.com>
2017-03-08 00:43:05 +0800

    xiu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {AlertModule} from 'ng2-bootstrap';
-
-import {AppComponent} from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule, JsonpModule} from "@angular/http";
+import {AlertModule} from "ng2-bootstrap";
+import {AppComponent} from "./app.component";
 import {LoginComponent} from "./hunter/login/login.component";
 import {AppRoutingModule} from "./app.routes";
-import { HunterComponent } from './hunter/hunter.component';
-import { CompanyComponent } from './hunter/company/company.component';
-import { ComsumeComponent } from './hunter/comsume/comsume.component';
-import { AssignComponent } from './hunter/assign/assign.component';
-import { SettingComponent } from './hunter/setting/setting.component';
+import {HunterComponent} from "./hunter/hunter.component";
+import {CompanyComponent} from "./hunter/company/company.component";
+import {ComsumeComponent} from "./hunter/comsume/comsume.component";
+import {AssignComponent} from "./hunter/assign/assign.component";
+import {SettingComponent} from "./hunter/setting/setting.component";
 
 @NgModule({
   declarations: [
@@ -27,10 +26,10 @@ import { SettingComponent } from './hunter/setting/setting.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    JsonpModule,
     AlertModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/hunter/comsume/comsume.component.ts
+++ src/app/hunter/comsume/comsume.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {Http, Response} from "@angular/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-comsume',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComsumeComponent implements OnInit {
 
-  constructor() { }
+  private url = 'http://localhost:9090/search/';
+  private unInterviewList: string[];
+  private todo: any;
+
+  constructor(private http: Http) {
+
+  }
 
   ngOnInit() {
+    this.getAllResumeWithStatus("待处理");
+  }
+
+  private getInfoByName() {
+    this.http.get(this.url).map(res => {
+
+    })
+  }
+
+  private getTopo10Resume() {
+
+  }
+
+  getAllResumeWithStatus(status: string) {
+    let statusUrl = this.url + "status/" + status
+    return this.http.get(statusUrl)
+      .map(this.extractData)
+      .catch(this.handleError)
+      .subscribe(resumes => this.unInterviewList = resumes);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || {};
+  }
+
+  private handleError(error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
 }
